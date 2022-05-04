35b3d4a706404e64664b4847931453787cbe1dd0
ErlynPino <johndoe@example.com>
2022-05-04 08:52:39 -0500

    pruebanotification

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
-importScripts("https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.23.0/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/7.3.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.3.0/firebase-messaging.js");
 
 firebase.initializeApp(
     {
-        apiKey: "AIzaSyAFtfVXeL9Oz0t1R4QTh0kUHXtPRQql04s",
-        authDomain: "fuckyncode.firebaseapp.com",
-        databaseURL: "https://fuckyncode.firebaseio.com",
-        projectId: "fuckyncode",
-        storageBucket: "fuckyncode.appspot.com",
-        messagingSenderId: "31417481552",
-        appId: "1:31417481552:web:94ec2797a0fb494aba1455"
+        apiKey: "AIzaSyA7098NZhk35lcOrYTXF2HmTW_v76XtqRo",
+        authDomain: "epical-ecb7f.firebaseapp.com",
+        databaseURL: "https://epical-ecb7f-default-rtdb.firebaseio.com",
+        projectId: "epical-ecb7f",
+        storageBucket: "epical-ecb7f.appsport.com",
+        messagingSenderId: "1066683181268",
+        appId: "1:1066683181268:web:76f7c9c1196d32f24e7341"
       }
 )
 const messaging= firebase.messaging();
